f62046da828ab43d0019be9d610e2a3f1296b6f4
Reece Walter <reece@reecewalter.com>
2024-06-07 08:11:26 -0400

    added "BASE_DIR" var to settings.py

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -15,6 +15,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
